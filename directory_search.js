21d3adbfc54e0494ffa4012d80c822c1d37a0a08
Jon Atxa <atxajon@gmail.com>
2019-06-05 23:20:37 +0200

    added directory search form interaction by passing in postcode values checking boxes and submitting form

--- directory_search.js
+++ directory_search.js
@@ -7,9 +7,7 @@ const postCode = 'AL1';
 void (async () => {
   let allData = [];
   try {
-    // create a new browser instance
     const browser = await puppeteer.launch({headless: false});
-    // const browser = await puppeteer.launch()
     
     // create a page inside the browser
     const page = await browser.newPage()
@@ -30,77 +28,32 @@ void (async () => {
     // console.log(JSON.stringify(cookiesSet));
     
     await page.waitFor('input[name=postcode]');
-    await page.$eval('input[name=postcode]', el => el.value = 'AL1');    
-    await page.click('.searchBtn');
-    // await page.waitFor('.ui-widget');
-    await page.waitForSelector('.searchBtn');
-    await page.waitFor(8000);
+    // Type into postcode input.
+    await page.focus('#input-8');
+    // @todo: replace hardcoded with array of values.
+    page.keyboard.type('AL1');
+    // Check investments checkbox.
+    await page.evaluate(()=>document.querySelector('#checkbox-12').click())
+    // Check pensions checkbox.
+    await page.evaluate(()=>document.querySelector('#checkbox-13').click())
+    // Click on search button.
+    await page.evaluate(()=>document.querySelector('.searchBtn').click())
 
+    await page.waitFor('#ResultSection');
+    await page.waitFor('.slds-select');
+    // @todo: Click on show all results.
+    await page.select('.slds-select', '200')
 
-    
-    // Uncomment if you need screengrabs.
-    await page.setViewport({ width: 1920, height: 1080 });
-    await page.screenshot({
-      path: './directory.png',
-      fullPage: true
-    })
+    await page.waitFor(19000);
     
     const html = await page.content();
     const fs = require('fs')
-    // @todo: replace hardcoded postcode for bulk processing of files.
     fs.writeFile(
       './test.html',
       html,
       (err) => err ? console.error('Data not written!', err) : console.log('Data written!')
     )
-    
-    
-    // Uncomment to generate a pdf of the page.
-    // await page.pdf({ path: './pdfs/page1.pdf' })
-    
-    // const individuals_string  = await page.evaluate(() => {
-    //   let individuals = '';
-    //   if ($('.panel-title a:contains("Individuals")').length) {
-    //     $('.panel-title a:contains("Individuals")').click();
-    //     var individualsRows = document.querySelectorAll('.SearchResultsTable tbody tr');
-    //     if (individualsRows) {
-    //       let individualCount = 0;
-    //       for(var i = 0; i < individualsRows.length; i++){              
-    //         let individual = '';
-    //         if (individualCount > 0) {
-    //           individual +=  individual + ' / ';
-    //         }
-    //         let name = individualsRows[i].querySelector("a").textContent;
-    
-    //         individual += name;
-    
-    //         let tds = individualsRows[i].getElementsByTagName("td");
-    //         let ind_ref_no = tds[1].textContent;
-    //         individual += '-' + ind_ref_no;
-    
-    //         let ind_status = tds[2].textContent;
-    //         individual += '-' + ind_status;
-    //         individuals += individual;
-    //         individualCount++;
-    //       }
-    //     } 
-    //     return individuals;
-    //   }
-    // });
-    
-    // Check if under permissions accordion tab there is permission to retail investment.
-    // const investmentsAdvice  = await page.evaluate(() => {
-    //   let advising_on_investments = false;
-    //   if ($('.panel-title a:contains("Permission")').length) {
-    //     $('.panel-title a:contains("Permission")').click();
-    //     if ($('li.PermCondsLimitationsItem:contains("Retail (Investment)")').length) {
-    //       advising_on_investments = true;
-    //     }                                                                                       
-    //   }
-    //   return advising_on_investments;
-    // });
-    
-    
+  
     // allData.push({
     //   ref_number: ref_no,
     //   individuals: individuals_string,
